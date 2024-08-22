4354d9211f236cfe8b0e2ec392a49db25d8331a5
Bartosz Kalinowski <kelostrada@gmail.com>
2024-08-22 16:19:58 +0200

    implement displaying replies to comments

--- lib/instagrain_web/live/post_live/comments_component.ex
+++ lib/instagrain_web/live/post_live/comments_component.ex
@@ -13,55 +13,111 @@ defmodule InstagrainWeb.PostLive.CommentsComponent do
     ~H"""
     <div class="text-sm">
       <%= unless @post.disable_comments do %>
-        <div :for={comment <- @post.comments} class="my-4">
-          <div class="flex gap-3">
-            <div>
-              <.avatar user={comment.user} size={:sm} />
-            </div>
-            <div class="grow">
-              <span class="font-bold">
-                <%= comment.user.username %>
-              </span>
-              <.time datetime={comment.inserted_at} />
-              <div>
-                <.user_content text={comment.comment} />
-              </div>
-            </div>
-            <div class="flex items-center">
-              <%= if comment.liked_by_current_user? do %>
-                <span
-                  phx-click="unlike-comment"
-                  phx-value-comment_id={comment.id}
-                  phx-target={@myself}
-                >
-                  <.icon
-                    name="hero-heart-solid"
-                    class="w-5 h-5 cursor-pointer hover:text-neutral-400 bg-red-500"
-                  />
-                </span>
-              <% else %>
-                <span phx-click="like-comment" phx-value-comment_id={comment.id} phx-target={@myself}>
-                  <.icon name="hero-heart" class="w-5 h-5 cursor-pointer hover:text-neutral-400" />
-                </span>
-              <% end %>
-            </div>
-          </div>
-          <div class="pt-2 px-11 flex gap-3">
-            <.comment_likes comment={comment} class="font-extrabold text-xs text-neutral-500" />
-            <.link
-              class="text-xs font-bold text-neutral-500"
-              phx-click="replyto"
-              phx-value-username={comment.user.username}
-              phx-value-comment_id={comment.id}
-              phx-target={"##{@comment_input_id}-container"}
-            >
-              Reply
-            </.link>
-          </div>
+        <div :for={comment <- @comments} class="my-4">
+          <.comment_component
+            comment={comment}
+            comment_input_id={@comment_input_id}
+            myself={@myself}
+            replies_shown={@replies_shown}
+          />
         </div>
       <% end %>
     </div>
     """
+  end
+
+  def comment_component(assigns) do
+    ~H"""
+    <div class="flex gap-3">
+      <div>
+        <.avatar user={@comment.user} size={:sm} />
+      </div>
+      <div class="grow">
+        <span class="font-bold">
+          <%= @comment.user.username %>
+        </span>
+        <.time datetime={@comment.inserted_at} />
+        <div>
+          <.user_content text={@comment.comment} />
+        </div>
+      </div>
+      <div class="flex items-center">
+        <%= if @comment.liked_by_current_user? do %>
+          <span phx-click="unlike-comment" phx-value-comment_id={@comment.id} phx-target={@myself}>
+            <.icon
+              name="hero-heart-solid"
+              class="w-5 h-5 cursor-pointer hover:text-neutral-400 bg-red-500"
+            />
+          </span>
+        <% else %>
+          <span phx-click="like-comment" phx-value-comment_id={@comment.id} phx-target={@myself}>
+            <.icon name="hero-heart" class="w-5 h-5 cursor-pointer hover:text-neutral-400" />
+          </span>
+        <% end %>
+      </div>
+    </div>
+    <div class="pt-2 pl-11 flex gap-3">
+      <.comment_likes comment={@comment} class="font-extrabold text-xs text-neutral-500" />
+      <.link
+        class="text-xs font-bold text-neutral-500"
+        phx-click="replyto"
+        phx-value-username={@comment.user.username}
+        phx-value-comment_id={@comment.reply_to_id || @comment.id}
+        phx-target={"##{@comment_input_id}-container"}
+      >
+        Reply
+      </.link>
+    </div>
+    <div :if={@comment.replies != [] && is_list(@comment.replies)} class="pt-2 pl-11">
+      <.link
+        :if={@comment.id not in @replies_shown}
+        class="text-xs font-bold text-neutral-500"
+        phx-click="display_replies"
+        phx-target={@myself}
+        phx-value-comment_id={@comment.id}
+      >
+        ---- View all <%= length(@comment.replies) %> replies
+      </.link>
+      <.link
+        :if={@comment.id in @replies_shown}
+        class="text-xs font-bold text-neutral-500"
+        phx-click="hide_replies"
+        phx-target={@myself}
+        phx-value-comment_id={@comment.id}
+      >
+        ---- Hide all replies
+      </.link>
+      <%= if @comment.id in @replies_shown do %>
+        <div :for={reply <- @comment.replies} class="my-4">
+          <.comment_component
+            comment={reply}
+            comment_input_id={@comment_input_id}
+            myself={@myself}
+            replies_shown={@replies_shown}
+          />
+        </div>
+      <% end %>
+    </div>
+    """
+  end
+
+  @impl true
+  def mount(socket) do
+    {:ok, assign(socket, replies_shown: [])}
+  end
+
+  @impl true
+  def update(assigns, socket) do
+    grouped_comments = Enum.group_by(assigns.post.comments, & &1.reply_to_id)
+
+    comments =
+      grouped_comments
+      |> Map.get(nil, [])
+      |> Enum.map(fn comment ->
+        %{comment | replies: Map.get(grouped_comments, comment.id, [])}
+      end)
+
+    {:ok, socket |> assign(assigns) |> assign(comments: comments)}
   end
 
   @impl true
@@ -111,6 +167,16 @@ defmodule InstagrainWeb.PostLive.CommentsComponent do
         notify_parent({:error, "Unlike failed"})
         {:noreply, socket}
     end
+  end
+
+  def handle_event("display_replies", %{"comment_id" => comment_id}, socket) do
+    comment_id = String.to_integer(comment_id)
+    {:noreply, assign(socket, replies_shown: [comment_id | socket.assigns.replies_shown])}
+  end
+
+  def handle_event("hide_replies", %{"comment_id" => comment_id}, socket) do
+    comment_id = String.to_integer(comment_id)
+    {:noreply, assign(socket, replies_shown: socket.assigns.replies_shown -- [comment_id])}
   end
 
   defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
